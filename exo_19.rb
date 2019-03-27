9c34e2f184e5bca104f2ad2991ad8af681d8bb20
Tahina93 <anne.tahina.metz@gmail.com>
2019-03-27 17:54:13 +0100

    exo 19

---
+++ exo_19.rb
@@ -0,0 +1,21 @@
+a = []
+
+i=0
+b = i+1
+
+51.times do |i|
+	 "jean.dupont.#{b}@email.fr"
+ 	a << "jean.dupont.#{b}@email.fr"
+ 		
+ 		if b.even? == true
+ 			puts "jean.dupont.#{b}@email.fr"
+
+		end
+	b = i+1
+end
+
+#puts "ce qu'il y a dans a"
+#puts a
+
+
+
