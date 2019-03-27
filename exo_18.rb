9aa0b6d83160bd99f45a62304eb9b6ec46b8818c
Tahina93 <anne.tahina.metz@gmail.com>
2019-03-27 15:01:26 +0100

    ajout de l'exo 18

---
+++ exo_18.rb
@@ -0,0 +1,13 @@
+a = []
+
+i=1
+
+50.times do |i|
+	 puts "jean.dupont.#{i+1}@email.fr"
+ a << "jean.dupont.#{i+1}@email.fr"
+
+end
+
+#derniere ligne pour vérifier ce qu'il y a dans mon array !
+
+puts a 
