43fee2bb29ff7f80f4697474002a014ecc623048
Tahina93 <anne.tahina.metz@gmail.com>
2019-03-27 13:15:52 +0100

    ajout exo 16

---
+++ exo_16.rb
@@ -0,0 +1,15 @@
+puts "Bonjour, quel âge as-tu ? "
+print "> "
+
+a = gets.chomp.to_i
+year_of_birth = 2019-a
+yob = year_of_birth-1
+
+b = a+1
+
+i = 1
+
+b.times do |i|
+	puts "#{year_of_birth+i} , il y a donc #{2019-(year_of_birth+i)} ans tu avais #{a-(2019-(year_of_birth+i))} ans !"
+
+	end
